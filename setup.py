695edf3dc641aaea669bbc173b9c64b08974f9c6
Dolph Mathews <dolph.mathews@gmail.com>
2016-02-02 21:08:55 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+import setuptools
+
+
+setuptools.setup(
+    name='rtox',
+    version='0.0.0',
+    description='Remote tox test runner',
+    author='Dolph Mathews',
+    author_email='dolph.mathews@gmail.com',
+    url='http://github.com/dolph/rtox',
+    scripts=['rtox.py'],
+    install_requires=['paramiko'],
+    py_modules=['rtox'],
+    entry_points={'console_scripts': ['rtox = rtox:main']},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+    ],
+)
